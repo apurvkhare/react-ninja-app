4104909d0e75a0387b9707f448f2082e7e7e1389
Apurv <apurvkhare1@gmail.com>
2019-08-03 21:41:44 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Ninjas from './Ninjas'
+
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app</h1>
+        <Ninjas ninjas={this.state.ninjas}/>
+      </div>
+    );
+  }
+}
+
+export default App;
